4fe466d8d1621b33e2866d72b73b128cf907088a
Lucas <lucrivdev@gmail.com>
2023-07-17 14:25:39 -0500

    Added support for weight and bias initialization technqiues: zero, ones, and random.

--- cool_net/neuron.py
+++ cool_net/neuron.py
@@ -1,21 +1,42 @@
 import numpy as np
 
 class Neuron:
-  def __init__(self, in_dim, bias=None):
-    self.weight_count = in_dim
-    self.w = np.random.randn(self.weight_count) * .01
-    self.b = bias
-    if self.b == None:
-      self.b = np.random.randn() * 0.01
+  techniques_weight_init = ["random"]
+  techniques_bias_init = ["zero","one"]
+  default_weight_init = techniques_weight_init[0]
+  default_bias_init = techniques_bias_init[0]
+
+  def __init__(self, 
+               input_dim, 
+               weight_init_technique="random",
+               bias_init_technique="zero"):
+    self.weight_init_technique = weight_init_technique.lower()
+    if weight_init_technique not in Neuron.techniques_weight_init:
+      print("weight initialization method not supported. using",Neuron.default_weight_init)
+      self.weight_init_technique = Neuron.default_bias_init
+    self.bias_init_technique = bias_init_technique.lower()
+    if bias_init_technique not in Neuron.techniques_bias_init:
+      print("bias initialization method not supported. using",Neuron.default_bias_init)
+      self.bias_init_technique = Neuron.default_bias_init
+
+    self.weight_count = input_dim
+    self.w = self._init_weights(self.weight_count, self.weight_init_technique)
+    self.b = self._init_bias(self.bias_init_technique)
+
+  def _init_weights(self, weight_count, init_technique):
+    if init_technique == "zero":
+      return np.zeros(weight_count)
+    if init_technique == "random":
+      return np.random.randn(weight_count) * .01
+    
+  def _init_bias(self, init_technique):
+    if init_technique == "zero":
+      return 0
+    if init_technique == "one":
+      return 1
 
   def forward(self, x):
     return np.dot(x,self.w) + self.b
-
-  def zeroWeights(self):
-    self.w = np.zeros(len(self.weight_count))
-
-  def zeroBias(self):
-    self.b = 0
 
   def loadWeights(self, weightsDict):
     if len(weightsDict) != self.weight_count:
@@ -26,11 +47,12 @@ class Neuron:
   def loadBias(self, bias_val):
     self.b = bias_val
 
+  def getWeights(self):
+    return self.w
+
   def getBias(self):
     return self.b
   
   def getWeightsDict(self):
     return {index: weight for index, weight in enumerate(self.w)}
   
-  def getWeightsList(self):
-    return self.w
